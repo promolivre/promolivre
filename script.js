38a8b16c58a64d59bd12bf93c4dbb1b73b0f933b
promolivre <joaoigor411@gmail.com>
2025-08-07 17:48:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -26,17 +26,4 @@ function getVisibleCount() {
 window.addEventListener('resize', updateCarousel);
 window.addEventListener('load', updateCarousel);
 
-heroTitle.style.opacity = 0;
-heroSubtitle.style.opacity = 0;
-
-setTimeout(() => {
-  heroTitle.textContent = title || defaultTitle;
-  heroSubtitle.textContent = subtitle || defaultSubtitle;
-
-  heroTitle.style.opacity = 1;
-  heroSubtitle.style.opacity = 1;
-}, 200);
-
-
 <script></script>
-
